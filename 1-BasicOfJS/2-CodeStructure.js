d023facc080d270f7e7ba8b30ce913212a82cac7
Haoyun Ji <haoyun.ji@microfocus.com>
2024-01-23 15:30:16 +0000

    day 2 commit

---
+++ 1-BasicOfJS/2-CodeStructure.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('Hello'); console.log('Hello again');
+console.log('Hello'); 
+console.log('Hello again');
+
+console.log(3 +
+  1
+  + 2);
+
+console.log('Hello')
+console.log('World')
